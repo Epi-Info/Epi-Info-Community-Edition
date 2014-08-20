70b5a7a51d2e9de1f562d52d52ff5ec6e95d0860
John Copeland <16403831+zfj4@users.noreply.github.com>
2014-08-20 17:24:50 +0000

    Dashboard Properties: Created LogisticProperties and LogisticParameters as copies of Means files.

---
+++ EpiDashboard/Gadgets/LogisticParameters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Epi;
+using Epi.Fields;
+
+namespace EpiDashboard
+{
+    public class LogisticParameters : FrequencyParametersBase
+    {
+        public bool ShowANOVA { get; set; }
+        public bool ShowMaxCol { get; set; }
+        public bool ShowModeCol { get; set; }
+        public bool ShowMeanCol { get; set; }
+        public bool ShowObservationsCol { get; set; }
+        public bool ShowVarianceCol { get; set; }
+        public bool ShowStdDevCol { get; set; }
+        public bool ShowMinCol { get; set; }
+        public bool Show25PercentCol { get; set; }
+        public bool ShowMedianCol { get; set; }
+        public bool Show75PercentCol { get; set; }
+        public bool ShowTotalCol { get; set; }
+        public List<int> columnsToHide;
+
+        public LogisticParameters()
+            : base()
+        {
+            ShowANOVA = true;
+            ShowMaxCol = true;
+            ShowModeCol = true;
+            ShowMeanCol = true;
+            ShowObservationsCol = true;
+            ShowVarianceCol = true;
+            ShowStdDevCol = true;
+            ShowMinCol = true;
+            Show25PercentCol = true;
+            ShowMedianCol = true;
+            Show75PercentCol = true;
+            ShowTotalCol = true;
+            columnsToHide = new List<int>();
+        }
+    }
+}
