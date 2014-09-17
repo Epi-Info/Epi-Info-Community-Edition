606db3d9028a61265cdd409d022f3d5c5b0d0ca7
den4_cp <SND\den4_cp>
2014-09-17 22:10:29 +0000

    Dashboard EpiCurveChart (HistogramChart): Initial Check-in for  Properties panel and Parameters objects.

---
+++ Epi Info 7/EpiDashboard/Controls/Charting/HistogramChartBase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ComponentArt.Win.DataVisualization.Charting;
+using EpiDashboard;
+using EpiDashboard.Gadgets.Charting;
+
+namespace EpiDashboard.Controls.Charting
+{
+    public class HistogramChartBase : ChartBase
+    {
+        public IChartSettings Settings { get; set; }
+        public HistogramChartParameters HistogramChartParameters { get; set; }
+        public DashboardHelper DashboardHelper { get; set; }
+
+        new protected virtual void SetLegendItems()
+        {
+            XYChart xyChart = null;
+            object el = FindName("xyChart");
+            if (el is XYChart)
+            {
+                xyChart = el as XYChart;
+            }
+
+            if (xyChart == null) throw new ApplicationException();
+
+            string sName = "";
+
+            if (HistogramChartParameters.StrataVariableNames.Count > 0)
+            {
+                foreach (Series s0 in xyChart.DataSeries)
+                {
+                    if (s0.Label != null)
+                    {
+                        sName = s0.Label.Split('.')[1];
+                        if (HistogramChartParameters.ShowLegendVarNames == false)
+                        {
+                            int index = sName.IndexOf(" = ");
+                            s0.Label = sName.Substring(index + 3);
+                        }
+                        else
+                        {
+                            s0.Label = sName;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (Series series in xyChart.DataSeries)
+                {
+                    if(series.Name == "series0")
+                    {
+                        series.Label = HistogramChartParameters.YAxisLabel;
+                    }
+                    else if(series.Name == "series1")
+                    {
+                        if (!string.IsNullOrEmpty(Y2AxisLegendTitle))
+                        {
+                            series.Label = HistogramChartParameters.Y2AxisLegendTitle;
+                        }
+                        else if (!string.IsNullOrEmpty(Y2AxisLabel))
+                        {
+                            series.Label = HistogramChartParameters.Y2AxisLabel;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Epi Info 7/EpiDashboard/Gadgets/HistogramChartParametersBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using ComponentArt.Win.DataVisualization;
+using ComponentArt.Win.DataVisualization.Common;
+using ComponentArt.Win.DataVisualization.Charting;
+
+namespace EpiDashboard
+{
+    public class HistogramChartParametersBase : ChartParametersBase
+    {
+        #region Events
+
+        #endregion //Events
+
+        #region Constructors
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public HistogramChartParametersBase()
+            : base()
+        {
+            BarSpace = BarSpacing.Default;
+            BarType = BarKind.Rectangle;
+        }
+
+        /// <summary>
+        /// Copy constructor
+        /// </summary>
+        public HistogramChartParametersBase(HistogramChartParametersBase parameters)
+            : base(parameters)
+        {
+            BarSpace = parameters.BarSpace;
+            BarType = parameters.BarType;
+        }
+        
+        #endregion     // Constructors
+
+        #region Properties
+        //IChartGadgetParameters Properties
+        
+        public BarSpacing BarSpace { get; set; }
+        public BarKind BarType { get; set; }
+        
+        #endregion  // Properties
+
+    }
+}
