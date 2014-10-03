d9f51621d7607d197b5f6cebf3771cdc3d2a7b02
sparitala_cp <SND\sparitala_cp>
2014-10-03 16:48:30 +0000

    7.1.3.44

--- Epi Info 7/StatisticsRepository/My Project/AssemblyInfo.vb
+++ Epi Info 7/StatisticsRepository/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("7.1.3.43")> 
-<Assembly: AssemblyFileVersion("7.1.3.43")> 
+<Assembly: AssemblyVersion("7.1.3.44")> 
+<Assembly: AssemblyFileVersion("7.1.3.44")> 
