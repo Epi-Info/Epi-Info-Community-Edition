33eb377a547a800933f749318657b99fbd9b3892
den4_cp <SND\den4_cp>
2014-10-07 19:00:56 +0000

    Dashboard Duplicates List gadget: Fixed gadget to show results when duplicates are exist; reduced size of lists.

--- Epi Info 7/EpiDashboard/Gadgets/Analysis/DuplicatesListControl.xaml.cs
+++ Epi Info 7/EpiDashboard/Gadgets/Analysis/DuplicatesListControl.xaml.cs
@@ -771,7 +771,9 @@ namespace EpiDashboard.Gadgets.Analysis
         /// </summary>
         public override void RefreshResults()
         {
-            if (!LoadingCombos && Parameters != null && Parameters.ColumnNames.Count > 0)
+            DuplicatesListParameters listParameters = (DuplicatesListParameters)Parameters;
+
+            if (!LoadingCombos && Parameters != null && listParameters.KeyColumnNames.Count > 0)
             {
                 if (IsHostedByEnter)
                 {
