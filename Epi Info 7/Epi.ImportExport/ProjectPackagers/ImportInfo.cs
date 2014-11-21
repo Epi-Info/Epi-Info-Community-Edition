c3f4be0425725cef3478c627d44246ee56c38701
Biohazard_cp <SND\Biohazard_cp>
2014-11-21 14:10:56 +0000

    Import/Export: Updated RowFilters to better support OR operation by properly reformatting conditions on Add operations Import/Export: Added collections to ImportInfo to track which records were inserted and updated for a given EI7 form during an import operation Import/Export: XmlDataUnpackager now tracks the IDs of records that underwent INSERT and UPDATE operations via the updated ImportInfo class. Import/Export: [branch] Updated XmlDataUnpackager to include certain interop improvements that were previously only included in the mainline.

--- Epi Info 7/Epi.ImportExport/ProjectPackagers/ImportInfo.cs
+++ Epi Info 7/Epi.ImportExport/ProjectPackagers/ImportInfo.cs
@@ -7,6 +7,9 @@ namespace Epi.ImportExport.ProjectPackagers
 {
     public class ImportInfo
     {
+        private Dictionary<View, List<string>> _recordIdsUpdated = new Dictionary<View, List<string>>();
+        private Dictionary<View, List<string>> _recordIdsAppended = new Dictionary<View, List<string>>();
+
         public int FormsProcessed;
         public int GridsProcessed;
         public int TotalRecordsUpdated;
@@ -31,6 +34,84 @@ namespace Epi.ImportExport.ProjectPackagers
             Messages = new ImportExportErrorList();
             UserID = string.Empty;
             Succeeded = false;
+        }
+
+        /// <summary>
+        /// Adds a record GUID to the list of record GUIDs that were updated during the import operation.
+        /// </summary>
+        /// <param name="form">The Epi Info 7 form that the updated record belongs to</param>
+        /// <param name="id">The GUID of the updated record</param>
+        public void AddRecordIdAsUpdated(View form, string id)
+        {
+            if (_recordIdsUpdated.ContainsKey(form))
+            {
+                List<string> recordIds = _recordIdsUpdated[form];
+                if (!recordIds.Contains(id))
+                {
+                    recordIds.Add(id);
+                }
+            }
+            else
+            {
+                _recordIdsUpdated.Add(form, new List<string>() { id });
+            }
+        }
+
+        /// <summary>
+        /// Adds a record GUID to the list of record GUIDs that were appended (inserted) during the import operation.
+        /// </summary>
+        /// <param name="form">The Epi Info 7 form that the appended record belongs to</param>
+        /// <param name="id">The GUID of the appended record</param>
+        public void AddRecordIdAsAppended(View form, string id)
+        {
+            if (_recordIdsAppended.ContainsKey(form))
+            {
+                List<string> recordIds = _recordIdsAppended[form];
+                if (!recordIds.Contains(id))
+                {
+                    recordIds.Add(id);
+                }
+            }
+            else
+            {
+                _recordIdsAppended.Add(form, new List<string>() { id });
+            }
+        }
+
+        /// <summary>
+        /// Gets a list of all of the records that were appended (inserted) during the import operation for the specified form
+        /// </summary>
+        /// <param name="form">The Epi Info 7 form for which the records were appended</param>
+        /// <returns>IEnumerable; a collection of strings that represent the GUIDs for each of the appended records</returns>
+        public IEnumerable<string> GetAppendedRecordIdsForForm(View form)
+        {
+            if (_recordIdsAppended.ContainsKey(form))
+            {
+                return _recordIdsAppended[form].AsEnumerable();
+            }
+
+            else
+            {
+                return new List<string>();
+            }
+        }
+
+        /// <summary>
+        /// Gets a list of all of the records that were updated during the import operation for the specified form
+        /// </summary>
+        /// <param name="form">The Epi Info 7 form for which the records were updated</param>
+        /// <returns>IEnumerable; a collection of strings that represent the GUIDs for each of the updated records</returns>
+        public IEnumerable<string> GetUpdatedRecordIdsForForm(View form)
+        {
+            if (_recordIdsUpdated.ContainsKey(form))
+            {
+                return _recordIdsUpdated[form].AsEnumerable();
+            }
+
+            else
+            {
+                return new List<string>();
+            }
         }
 
         public void AddNotification(string message, string code)
