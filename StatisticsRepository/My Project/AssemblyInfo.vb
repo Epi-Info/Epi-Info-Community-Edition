e29ce89a6b5b08e04b23e894c93d8dd5927e9055
sparitala_cp <18400641+sparitala@users.noreply.github.com>
2015-03-10 17:51:19 +0000

    7.1.5.0

--- StatisticsRepository/My Project/AssemblyInfo.vb
+++ StatisticsRepository/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("7.1.4.19")> 
-<Assembly: AssemblyFileVersion("7.1.4.19")> 
+<Assembly: AssemblyVersion("7.1.5.0")> 
+<Assembly: AssemblyFileVersion("7.1.5.0")> 
