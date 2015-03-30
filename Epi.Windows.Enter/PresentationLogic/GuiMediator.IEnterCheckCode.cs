6b1e5e2dc379e6d8e45cc4799e79080b9d6e2efd
Gsunki_cp <SND\Gsunki_cp>
2015-03-30 16:54:55 +0000

    Added changes to NewRecord

--- Epi.Windows.Enter/PresentationLogic/GuiMediator.IEnterCheckCode.cs
+++ Epi.Windows.Enter/PresentationLogic/GuiMediator.IEnterCheckCode.cs
@@ -1298,20 +1298,16 @@ namespace Epi.Windows.Enter.PresentationLogic
                 this.mainForm.CloseView();
                 return;
             }
-           // ResetViewDataFields();
-            if (AllowOneRecordOnly) { return; }
-
-            this.EnterCheckCodeEngine.CheckCodeHandler(this, new Epi.EnterCheckCodeEngine.RunCheckCodeEventArgs(Epi.EnterCheckCodeEngine.EventActionEnum.NewRecord, "+"));
+            ResetViewDataFields();
             this.canvas.UnsubscribeControlEventHandlers();
             this.CurrentRecordId = this.EnterCheckCodeEngine.CurrentView.CurrentRecordNumber;
             this.canvas.SetNewRecordValues();
             this.mainForm.UpdateAppSpecificInfo(SharedStrings.NEW_RECORD);
             this.mainForm.AddNewRecordSettings();
             this.viewExplorer.GoToFirstPage();
-            this.OpenPageHandler(this, new PageSelectedEventArgs(this.currentPage));    
             this.Render();
-            //this.canvas.SubscribeControlEventHandlers();
-            //SetFocusToFirstControl(currentPage, _fieldPanel);
+            this.canvas.SubscribeControlEventHandlers();
+            SetFocusToFirstControl(currentPage, _fieldPanel);
             }
         }
 
