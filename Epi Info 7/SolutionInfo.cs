2d3b16524cccd52bd46697bceb4efc80d91711b6
sparitala_cp <SND\sparitala_cp>
2015-07-22 13:58:50 +0000

    7.1.6.8

--- Epi Info 7/SolutionInfo.cs
+++ Epi Info 7/SolutionInfo.cs
@@ -37,10 +37,10 @@ using Epi;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("7.1.*")]
-[assembly: AssemblyFileVersion("7.1.6.7")]
-[assembly: AssemblyInformationalVersion("7.1.6.7")]
+[assembly: AssemblyFileVersion("7.1.6.8")]
+[assembly: AssemblyInformationalVersion("7.1.6.8")]
 [assembly: SatelliteContractVersion("7.0.0.0")]
-[assembly: Epi.AssemblyReleaseDateAttribute("07/07/2015")]
+[assembly: Epi.AssemblyReleaseDateAttribute("07/21/2015")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
