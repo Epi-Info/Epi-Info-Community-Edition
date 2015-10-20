3695ab918aece03aff8693c89ab61e7d29c91a2c
den4_cp <25185636+davidnitschke@users.noreply.github.com>
2015-10-20 18:57:12 +0000

    EI-490 Fixed BaseReadDialog and TimeConfigDialog for translation.

--- EpiDashboard/Mapping/TimeConfigDialog.Designer.cs
+++ EpiDashboard/Mapping/TimeConfigDialog.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimeConfigDialog));
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnOk = new System.Windows.Forms.Button();
             this.cbxTime = new System.Windows.Forms.ComboBox();
@@ -37,22 +38,15 @@
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(174, 66);
+            resources.ApplyResources(this.btnCancel, "btnCancel");
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
-            this.btnCancel.TabIndex = 11;
-            this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnOk
             // 
-            this.btnOk.Enabled = false;
-            this.btnOk.Location = new System.Drawing.Point(93, 66);
+            resources.ApplyResources(this.btnOk, "btnOk");
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 23);
-            this.btnOk.TabIndex = 10;
-            this.btnOk.Text = "OK";
             this.btnOk.UseVisualStyleBackColor = true;
             this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
@@ -60,28 +54,21 @@
             // 
             this.cbxTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbxTime.FormattingEnabled = true;
-            this.cbxTime.Location = new System.Drawing.Point(15, 25);
+            resources.ApplyResources(this.cbxTime, "cbxTime");
             this.cbxTime.Name = "cbxTime";
-            this.cbxTime.Size = new System.Drawing.Size(234, 21);
-            this.cbxTime.TabIndex = 9;
             this.cbxTime.SelectedIndexChanged += new System.EventHandler(this.cbxTime_SelectedIndexChanged);
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Time Variable:";
             // 
             // TimeConfigDialog
             // 
             this.AcceptButton = this.btnOk;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(261, 101);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.cbxTime);
@@ -90,10 +77,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "TimeConfigDialog";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Configure Time";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
