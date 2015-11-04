9fdf3d54d970d61c6e67364db2a866ab719300c5
ita3_cp <SND\ita3_cp>
2015-11-04 13:16:01 +0000

    7.1.6.1404

--- Epi Info 7/SolutionInfo.cs
+++ Epi Info 7/SolutionInfo.cs
@@ -37,10 +37,10 @@ using Epi;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("7.1.*")]
-[assembly: AssemblyFileVersion("7.1.6.1403")]
-[assembly: AssemblyInformationalVersion("7.1.6.1403")]
+[assembly: AssemblyFileVersion("7.1.6.1404")]
+[assembly: AssemblyInformationalVersion("7.1.6.1404")]
 [assembly: SatelliteContractVersion("7.0.0.0")]
-[assembly: Epi.AssemblyReleaseDateAttribute("10/20/2015")]
+[assembly: Epi.AssemblyReleaseDateAttribute("11/04/2015")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- Epi Info 7/StatisticsRepository/My Project/AssemblyInfo.vb
+++ Epi Info 7/StatisticsRepository/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("7.1.6.1403")> 
-<Assembly: AssemblyFileVersion("7.1.6.1403")> 
+<Assembly: AssemblyVersion("7.1.6.1404")> 
+<Assembly: AssemblyFileVersion("7.1.6.1404")> 
