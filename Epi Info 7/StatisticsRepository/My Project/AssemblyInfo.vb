7752e9bae42fc941707a0b79651ba8b5b1279b35
ita3_cp <SND\ita3_cp>
2015-11-30 13:48:11 +0000

    7.1.6.1602

--- Epi Info 7/StatisticsRepository/My Project/AssemblyInfo.vb
+++ Epi Info 7/StatisticsRepository/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("7.1.6.1601")> 
-<Assembly: AssemblyFileVersion("7.1.6.1601")> 
+<Assembly: AssemblyVersion("7.1.6.1602")> 
+<Assembly: AssemblyFileVersion("7.1.6.1602")> 
