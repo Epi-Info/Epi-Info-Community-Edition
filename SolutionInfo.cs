9199b0f913fa7955201d839ef8c6921cbffd4421
ita3_cp <18171996+cdc-dpbrown@users.noreply.github.com>
2015-12-10 12:27:23 +0000

    7.1.6.1701

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -37,10 +37,10 @@ using Epi;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("7.1.*")]
-[assembly: AssemblyFileVersion("7.1.6.17")]
-[assembly: AssemblyInformationalVersion("7.1.6.17")]
+[assembly: AssemblyFileVersion("7.1.6.1701")]
+[assembly: AssemblyInformationalVersion("7.1.6.1701")]
 [assembly: SatelliteContractVersion("7.0.0.0")]
-[assembly: Epi.AssemblyReleaseDateAttribute("12/02/2015")]
+[assembly: Epi.AssemblyReleaseDateAttribute("12/10/2015")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
