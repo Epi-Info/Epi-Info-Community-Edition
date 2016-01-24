7e0f1283395630e8d4ef17656eac9f5f5832e832
sparitala_cp <18400641+sparitala@users.noreply.github.com>
2016-01-24 21:48:44 +0000

    7.1.6.2001

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -40,7 +40,7 @@ using Epi;
 [assembly: AssemblyFileVersion("7.1.6.2001")]
 [assembly: AssemblyInformationalVersion("7.1.6.2001")]
 [assembly: SatelliteContractVersion("7.0.0.0")]
-[assembly: Epi.AssemblyReleaseDateAttribute("1/24/2015")]
+[assembly: Epi.AssemblyReleaseDateAttribute("1/24/2016")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
