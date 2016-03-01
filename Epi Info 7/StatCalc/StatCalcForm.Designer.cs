b745855ab8168a86e668354a1377216a53325e81
jkhoosal_cp <SND\jkhoosal_cp>
2016-03-01 17:49:25 +0000

    UI Fixes and changes

--- Epi Info 7/StatCalc/StatCalcForm.Designer.cs
+++ Epi Info 7/StatCalc/StatCalcForm.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.ClientSize = new System.Drawing.Size(680,500);
+            this.ClientSize = new System.Drawing.Size(680,455);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "StatCalcForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
