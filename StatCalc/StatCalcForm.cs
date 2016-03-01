b9acfaf9464b3b0aad031f6dfd33ec9491200b2a
jkhoosal_cp <jkhoosal@users.noreply.github.com>
2016-03-01 17:35:45 +0000

    Splashpage /StatCalc fixes

--- StatCalc/StatCalcForm.cs
+++ StatCalc/StatCalcForm.cs
@@ -17,12 +17,26 @@ namespace StatCalc
         public StatCalcForm()
         {
             InitializeComponent();
+            //this.MaximumSize = new System.Drawing.Size(680, 500);
+            //this.MinimumSize = new System.Drawing.Size(680, 500);
+            //this.Width++;
+            //this.Width--;
+            //this.Height++;
+            if (this.Width == 526 && Height == 445)
+            {
+                this.Size = new Size(550,405);
+            }
+
+            //this.MaximumSize = new System.Drawing.Size(680, 500);
+            //this.MinimumSize = new System.Drawing.Size(680, 500);
             host = new ElementHost();
             host.Dock = DockStyle.Fill;
             StatCalcMenu form = new StatCalcMenu();
             host.Child = form;
             this.Controls.Add(host);
         }
+
+        
 
        
 
