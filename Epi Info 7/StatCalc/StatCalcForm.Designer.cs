93b7543261bf469e0d8f2ca0573d7135b872e86e
jkhoosal_cp <SND\jkhoosal_cp>
2016-03-01 17:35:45 +0000

    Splashpage /StatCalc fixes

--- Epi Info 7/StatCalc/StatCalcForm.Designer.cs
+++ Epi Info 7/StatCalc/StatCalcForm.Designer.cs
@@ -35,12 +35,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(680,500);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "StatCalcForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "StatCalc";
-            this.MaximumSize = new System.Drawing.Size(680, 500);
-            this.MinimumSize = new System.Drawing.Size(680, 500);
+          
             this.ResumeLayout(false);
 
         }
