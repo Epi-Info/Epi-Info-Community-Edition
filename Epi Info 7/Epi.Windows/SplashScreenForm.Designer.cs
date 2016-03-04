eabbf8738420f06a202644232e1f509616ae0025
sparitala_cp <SND\sparitala_cp>
2016-03-04 16:32:43 +0000

    Loading splashicon changes.

--- Epi Info 7/Epi.Windows/SplashScreenForm.Designer.cs
+++ Epi Info 7/Epi.Windows/SplashScreenForm.Designer.cs
@@ -68,7 +68,7 @@
             // 
             // SplashScreenForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+           // this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(395, 444);
             this.Controls.Add(this.label2);
