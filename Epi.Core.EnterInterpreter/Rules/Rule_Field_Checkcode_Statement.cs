0026bc64fb48ef1dc7abd25616e76b293a762453
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2016-03-08 23:46:53 +0000

    EI-797

--- Epi.Core.EnterInterpreter/Rules/Rule_Field_Checkcode_Statement.cs
+++ Epi.Core.EnterInterpreter/Rules/Rule_Field_Checkcode_Statement.cs
@@ -23,14 +23,17 @@ namespace Epi.Core.EnterInterpreter.Rules
             this.TextField = this.ExtractTokensWithFormat(pToken.Tokens);
 
             this.Identifier = this.GetCommandElement(pToken.Tokens, 1).Trim(new char[] { '[',']'});
-            if (!string.IsNullOrEmpty(Identifier))
+            if (pContext.IsVariableValidationEnable)
             {
-
-                if (!this.Context.CommandVariableCheck.ContainsKey(Identifier.ToLower()))
+                if (!string.IsNullOrEmpty(Identifier))
                 {
-                    this.Context.CommandVariableCheck.Add(Identifier, "Field");
-                }
 
+                    if (!this.Context.CommandVariableCheck.ContainsKey(Identifier.ToLower()))
+                    {
+                        this.Context.CommandVariableCheck.Add(Identifier, "Field");
+                    }
+
+                }
             }
             if (Context.ParsedFieldNames.Contains(Identifier.ToLower()) == false)
             {
