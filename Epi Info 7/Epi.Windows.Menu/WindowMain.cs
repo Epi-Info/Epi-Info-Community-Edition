429c36b5e0f4ca50497a5ff4d1d44d1318161e64
jkhoosal_cp <SND\jkhoosal_cp>
2016-03-17 14:29:05 +0000

    UI Changes

--- Epi Info 7/Epi.Windows.Menu/WindowMain.cs
+++ Epi Info 7/Epi.Windows.Menu/WindowMain.cs
@@ -19,15 +19,15 @@ namespace Epi.Windows.Menu
         public WindowMain() 
         {
             InitializeComponent();          
-            this.MaximumSize = new System.Drawing.Size(857, 745);
-            this.MinimumSize = new System.Drawing.Size(500, 400);
+            //this.MaximumSize = new System.Drawing.Size(857, 745);
+            //this.MinimumSize = new System.Drawing.Size(500, 400);
            
             if (this.ClientSize.Width >840 && this.ClientSize.Height> 700)
             {
                 this.Size = new Size(700, 599);
 
-                this.MaximumSize = new System.Drawing.Size(700, 599);
-                this.MinimumSize = new System.Drawing.Size(450, 400);
+                //this.MaximumSize = new System.Drawing.Size(700, 599);
+                //this.MinimumSize = new System.Drawing.Size(450, 400);
             }
                      
             host = new ElementHost();
