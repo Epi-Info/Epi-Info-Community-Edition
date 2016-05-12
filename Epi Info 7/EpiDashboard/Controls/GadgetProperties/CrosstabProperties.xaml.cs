38a3e82c3355bae9acb27ee1f07bd85e78cefd99
zfj4_cp <SND\zfj4_cp>
2016-05-12 12:49:14 +0000

    EI-859: Made the stratification variables box in Dashboard Tables shorter by 100.

--- Epi Info 7/EpiDashboard/Controls/GadgetProperties/CrosstabProperties.xaml.cs
+++ Epi Info 7/EpiDashboard/Controls/GadgetProperties/CrosstabProperties.xaml.cs
@@ -382,7 +382,7 @@ namespace EpiDashboard.Controls.GadgetProperties
             }
             cbxOutcomeField.SelectedItem = Parameters.CrosstabVariableName;
             cbxFieldWeight.SelectedItem = Parameters.WeightVariableName;
-            lbxFieldStrata.MaxHeight = lbxFieldStrata.MaxHeight + (System.Windows.SystemParameters.PrimaryScreenHeight - 768.0);
+            lbxFieldStrata.MaxHeight = lbxFieldStrata.MaxHeight + (System.Windows.SystemParameters.PrimaryScreenHeight - 868.0);
             scrollViewerStrataProperties.Height = scrollViewerStrataProperties.Height + (System.Windows.SystemParameters.PrimaryScreenHeight - 768.0);
             if (Parameters.StrataVariableNames.Count > 0)
             {
