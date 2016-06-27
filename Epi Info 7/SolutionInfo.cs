ae1c6b7abcef8104084835b967a2bd9eca536986
ita3_cp <SND\ita3_cp>
2016-06-27 19:33:06 +0000

    06/27/2016

--- Epi Info 7/SolutionInfo.cs
+++ Epi Info 7/SolutionInfo.cs
@@ -40,7 +40,7 @@ using Epi;
 [assembly: AssemblyFileVersion("7.2.0.1")]
 [assembly: AssemblyInformationalVersion("7.2.0.1")]
 [assembly: SatelliteContractVersion("7.0.0.0")]
-[assembly: Epi.AssemblyReleaseDateAttribute("06/23/2016")]
+[assembly: Epi.AssemblyReleaseDateAttribute("06/27/2016")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
