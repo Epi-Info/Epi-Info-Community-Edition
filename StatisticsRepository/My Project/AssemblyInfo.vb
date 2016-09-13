8b87f91ac4475e6e8e27da742bc6e0122f7017ae
ita3_cp <18171996+cdc-dpbrown@users.noreply.github.com>
2016-09-13 17:20:35 +0000

    [BUILD] > 7.2.0.2 09/13/2016

--- StatisticsRepository/My Project/AssemblyInfo.vb
+++ StatisticsRepository/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("6edf24ca-6d16-41e5-90e1-0f7bf75676a0")> 
+<Assembly: Guid("6edf24ca-6d16-41e5-90e1-0f7bf75676a0")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("7.2.0.1")> 
-<Assembly: AssemblyFileVersion("7.2.0.1")> 
+<Assembly: AssemblyVersion("7.2.0.2")>
+<Assembly: AssemblyFileVersion("7.2.0.2")>
