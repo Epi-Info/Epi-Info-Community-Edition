e284fcc718075f552efa9a2d5f30f798b61cf211
ita3_cp <SND\ita3_cp>
2016-10-19 13:01:26 +0000

    Added googleapis.com/geolocation to SYSLAT and SYSLON in check code.

--- Epi Info 7/Epi.Core.EnterInterpreter/Rules/Functions/Rule_SystemLongitude.cs
+++ Epi Info 7/Epi.Core.EnterInterpreter/Rules/Functions/Rule_SystemLongitude.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Device.Location;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using com.calitha.goldparser;
+using System.Net;
+using System.Collections.Specialized;
 
 namespace Epi.Core.EnterInterpreter.Rules
 {
@@ -34,6 +34,29 @@ namespace Epi.Core.EnterInterpreter.Rules
             }
             else
             {
+                try
+                {
+                    string url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCkwERu0dtXN0CARM8DQNE_12-JiDsF2OM";
+                    string rs = "";
+
+                    using (var client = new WebClient())
+                    {
+                        var values = new NameValueCollection();
+                        var response = client.UploadValues(url, values);
+                        rs = System.Text.Encoding.Default.GetString(response);
+                    }
+
+                    int latIndex = rs.IndexOf("lng");
+                    string lat = rs.Substring(latIndex + 5, 18).Trim();
+
+                    double checkLat;
+                    if (double.TryParse(lat, out checkLat))
+                    {
+                        return lat;
+                    }
+                }
+                catch { }
+
                 return null;
             }
         }
