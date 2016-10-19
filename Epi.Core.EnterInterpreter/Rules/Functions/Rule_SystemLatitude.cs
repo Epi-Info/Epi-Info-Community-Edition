da3e40fa2faa004abf5ea17767923a6803f8a65a
ita3_cp <18171996+cdc-dpbrown@users.noreply.github.com>
2016-10-19 13:01:26 +0000

    Added googleapis.com/geolocation to SYSLAT and SYSLON in check code.

--- Epi.Core.EnterInterpreter/Rules/Functions/Rule_SystemLatitude.cs
+++ Epi.Core.EnterInterpreter/Rules/Functions/Rule_SystemLatitude.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Device.Location;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using com.calitha.goldparser;
+using System.Net;
+using System.Collections.Specialized;
 
 namespace Epi.Core.EnterInterpreter.Rules
 {
@@ -34,8 +34,106 @@ namespace Epi.Core.EnterInterpreter.Rules
             }
             else
             {
+                try
+                {
+                    string url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCkwERu0dtXN0CARM8DQNE_12-JiDsF2OM";
+                    string rs = "";
+
+                    using (var client = new WebClient())
+                    {
+                        var values = new NameValueCollection();
+                        var response = client.UploadValues(url, values);
+                        rs = System.Text.Encoding.Default.GetString(response);
+                    }
+
+                    int latIndex = rs.IndexOf("lat");
+                    string lat = rs.Substring(latIndex + 5, 18).Trim();
+
+                    double checkLat;
+                    if(double.TryParse(lat, out checkLat))
+                    {
+                        return lat;
+                    }
+                }
+                catch { }
+
                 return null;
             }
         }
+    }
+    public class Address
+    {
+        public string name;
+        public Status Status;
+        public IList<Placemark> Placemark;
+    }
+
+    public class Status
+    {
+        public int code;
+        public string request;
+    }
+
+    public class Placemark
+    {
+        public string id;
+        public string address;
+        public AddressDetails AddressDetails;
+        public ExtendedData ExtendedData;
+        public Point Point;
+    }
+
+    public class AddressDetails
+    {
+        public int Accuracy;
+        public Country Country;
+    }
+
+    public class ExtendedData
+    {
+        public LatLonBox LatLonBox;
+    }
+
+    public class Point
+    {
+        public IList<double> coordinates;
+    }
+
+    public class Country
+    {
+        public AdministrativeArea AdministrativeArea;
+        public string CountryName;
+        public string CountryNameCode;
+    }
+
+    public class LatLonBox
+    {
+        public double north;
+        public double south;
+        public double east;
+        public double west;
+    }
+
+    public class AdministrativeArea
+    {
+        public string AdministrativeAreaName;
+        public Locality Locality;
+    }
+
+    public class Locality
+    {
+        public string LocalityName;
+        public PostalCode PostalCode;
+        public Thoroughfare Thoroughfare;
+    }
+
+    public class PostalCode
+    {
+        public string PostalCodeNumber;
+    }
+
+    public class Thoroughfare
+    {
+        public string ThoroughfareName;
     }
 }
