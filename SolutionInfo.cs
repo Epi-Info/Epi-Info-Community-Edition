1f94cf3cf1cd108c27bef9a359d4ee6771bd1910
ita3_cp <18171996+cdc-dpbrown@users.noreply.github.com>
2017-01-13 16:51:50 +0000

    [BUILD] 7.2.1 > 7.2.1.0

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -37,8 +37,8 @@ using Epi;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("7.2.*")]
-[assembly: AssemblyFileVersion("7.2.1")]
-[assembly: AssemblyInformationalVersion("7.2.1")]
+[assembly: AssemblyFileVersion("7.2.1.0")]
+[assembly: AssemblyInformationalVersion("7.2.1.0")]
 [assembly: SatelliteContractVersion("7.0.0.0")]
 [assembly: Epi.AssemblyReleaseDateAttribute("1/12/2017")]
 
