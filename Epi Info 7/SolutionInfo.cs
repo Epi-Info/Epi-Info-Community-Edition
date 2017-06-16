8e1d57fc02afccfd2b67149c10b1c0f9ea8682a1
ita3_cp <SND\ita3_cp>
2017-06-16 14:39:33 +0000

    [BUILD] 7.2.1.11 6/16/2017

--- Epi Info 7/SolutionInfo.cs
+++ Epi Info 7/SolutionInfo.cs
@@ -37,10 +37,10 @@ using Epi;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("7.2.*")]
-[assembly: AssemblyFileVersion("7.2.1.10")]
-[assembly: AssemblyInformationalVersion("7.2.1.10")]
+[assembly: AssemblyFileVersion("7.2.1.11")]
+[assembly: AssemblyInformationalVersion("7.2.1.11")]
 [assembly: SatelliteContractVersion("7.0.0.0")]
-[assembly: Epi.AssemblyReleaseDateAttribute("06/06/2017")]
+[assembly: Epi.AssemblyReleaseDateAttribute("06/16/2017")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
