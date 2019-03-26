567d71a76151e1ab8d3a3fa0ac2685e0ada8bdc0
cdc-dpbrown <18171996+cdc-dpbrown@users.noreply.github.com>
2019-03-26 11:40:27 -0400

    [BUILD] 7.2.2.19 3/26/2019

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -37,10 +37,10 @@ using Epi;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("7.2.*")]
-[assembly: AssemblyFileVersion("7.2.2.18")]
-[assembly: AssemblyInformationalVersion("7.2.2.18")]
+[assembly: AssemblyFileVersion("7.2.2.19")]
+[assembly: AssemblyInformationalVersion("7.2.2.19")]
 [assembly: SatelliteContractVersion("7.0.0.0")]
-[assembly: Epi.AssemblyReleaseDateAttribute("03/21/2019")]
+[assembly: Epi.AssemblyReleaseDateAttribute("03/26/2019")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- StatisticsRepository/My Project/AssemblyInfo.vb
+++ StatisticsRepository/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("7.2.2.18")>
-<Assembly: AssemblyFileVersion("7.2.2.18")>
+<Assembly: AssemblyVersion("7.2.2.19")>
+<Assembly: AssemblyFileVersion("7.2.2.19")>
