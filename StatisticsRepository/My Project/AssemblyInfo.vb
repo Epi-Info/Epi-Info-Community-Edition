989619dda9f28fb58fff426f4e5a831bcbded1b6
cdc-dpbrown <ita3@cdc.gov>
2019-10-15 20:30:52 -0400

    [BUILD] 7.2.3.6 10/15/2019

--- StatisticsRepository/My Project/AssemblyInfo.vb
+++ StatisticsRepository/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("7.2.3.5")>
-<Assembly: AssemblyFileVersion("7.2.3.5")>
+<Assembly: AssemblyVersion("7.2.3.6")>
+<Assembly: AssemblyFileVersion("7.2.3.6")>
