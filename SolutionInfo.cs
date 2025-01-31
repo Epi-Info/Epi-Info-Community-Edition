0ce825d38b6871b2eba5c23421ee72e99926987a
cdc-dpbrown <ita3@cdc.gov>
2025-01-31 16:37:24 -0500

    [BUILD] 7.2.6.21 1/31/2025

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -37,10 +37,10 @@ using Epi;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("7.2.*")]
-[assembly: AssemblyFileVersion("7.2.6.20")]
-[assembly: AssemblyInformationalVersion("7.2.6.20")]
+[assembly: AssemblyFileVersion("7.2.6.21")]
+[assembly: AssemblyInformationalVersion("7.2.6.21")]
 [assembly: SatelliteContractVersion("7.0.0.0")]
-[assembly: Epi.AssemblyReleaseDateAttribute("01/08/2025")]
+[assembly: Epi.AssemblyReleaseDateAttribute("01/31/2025")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
