41639034e031b39048e01bbd6da4c7d9d21e0a91
cdc-dpbrown <ita3@cdc.gov>
2025-02-11 14:09:59 -0500

    [BUILD] 7.2.7.0 2/11/2025

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -37,10 +37,10 @@ using Epi;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("7.2.*")]
-[assembly: AssemblyFileVersion("7.2.6.21")]
-[assembly: AssemblyInformationalVersion("7.2.6.21")]
+[assembly: AssemblyFileVersion("7.2.7.0")]
+[assembly: AssemblyInformationalVersion("7.2.7.0")]
 [assembly: SatelliteContractVersion("7.0.0.0")]
-[assembly: Epi.AssemblyReleaseDateAttribute("01/31/2025")]
+[assembly: Epi.AssemblyReleaseDateAttribute("02/11/2025")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- StatisticsRepository/My Project/AssemblyInfo.vb
+++ StatisticsRepository/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("7.2.6.21")>
-<Assembly: AssemblyFileVersion("7.2.6.21")>
+<Assembly: AssemblyVersion("7.2.7.0")>
+<Assembly: AssemblyFileVersion("7.2.7.0")>
